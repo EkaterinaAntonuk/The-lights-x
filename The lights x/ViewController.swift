e75c0d5598aa32f47afa76dc0d98601279d574cb
Ekaterina Kuznetsova <eantonuk@gmail.com>
2022-01-11 19:55:55 +0300

    Initial Commit

---
+++ The lights x/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  The lights x
+//
+//  Created by Ekaterina Kuznetsova on 11.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
