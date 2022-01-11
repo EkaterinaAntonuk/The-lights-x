5bf9dd4a6f8dee202b376c5f50e861cd729986fb
Ekaterina Kuznetsova <eantonuk@gmail.com>
2022-01-11 20:45:49 +0300

    Add color switch for background and code of physical light of device

--- The lights x/ViewController.swift
+++ The lights x/ViewController.swift
@@ -6,14 +6,51 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    var lightColor = 1
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
+    @IBAction func buttonPressed() {
+        switch lightColor {
+                case 1:
+                    view.backgroundColor = .red
+                case 2:
+                    view.backgroundColor = .yellow
+                case 3:
+                    view.backgroundColor = .purple
+                case 4:
+                    view.backgroundColor = .blue
+                default:
+                    view.backgroundColor = .red
+                    lightColor = 0
+                }
+                lightColor += 1
+    }
+    
+    func toggleTorch(on: Bool) {
+        guard let device = AVCaptureDevice.default(for: AVMediaType.video) else { return }
+        guard device.hasTorch else { print("Torch isn't available"); return }
 
+        do {
+            try device.lockForConfiguration()
+            device.torchMode = on ? .on : .off
+            if on { try device.setTorchModeOn(level: AVCaptureDevice.maxAvailableTorchLevel.significand) }
+            device.unlockForConfiguration()
+        } catch {
+            print("Torch can't be used")
+        }
+    }
+    
 }
 
